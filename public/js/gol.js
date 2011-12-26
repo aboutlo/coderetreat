650d6d85b2ea060c65b02204dba198fceaba7fe5
aboutlo <lorenzo.sicilia@gmail.com>
2011-12-26 16:59:53 +0000

    gol with all rules implemented

--- public/js/gol.js
+++ public/js/gol.js
@@ -2,24 +2,25 @@
 function Cell (status){
     this.status = status;
 }
+
 Cell.ALIVE = true;
 Cell.DEAD = false;
 
 Cell.prototype.isAlive = function(){
     return this.status;
-}
+};
 
 Cell.prototype.evolve = function(neighbors){
 
-    var neighborsAlive = _.filter(neighbors, function(neighbor){ return neighbor.isAlive() }).length;
+    var aliveNeighbors = _.filter(neighbors, function(neighbor){ return neighbor.isAlive(); }).length;
 
-    if(this.isAlive() &&  neighbors.length >= 2 && neighbors.length <= 3 ){
+    if(this.isAlive() && aliveNeighbors  >= 2 && aliveNeighbors  <= 3 ){
         return new Cell(Cell.ALIVE) ;
     }
 
-    if( !this.isAlive() &&  neighbors.length == 3){
+    if( this.isAlive() === false &&  aliveNeighbors == 3){
         return new Cell(Cell.ALIVE);
     }
-
+    
     return new Cell(Cell.DEAD);
-}
+};
